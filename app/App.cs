8126e728a61981b51313a2a30ef653e8a547baf4
Jacob-Gruver <gruvinbro@gmail.com>
2021-06-18 16:05:29 -0600

    init

--- app/App.cs
+++ app/App.cs
@@ -34,11 +34,12 @@ namespace math_opp
             switch (choice)
             {
                 case "pt":
-                    var instance = new PythTheorem(this);
-                    instance.pythTheorem();
+                    var inst1 = new PythTheorem(this);
+                    inst1.pythTheorem();
                     break;
                 case "om":
-                    ohmLaw();
+                    var inst2 = new OhmLaw(this);
+                    inst2.ohmLaw();
                     break;
                 default:
                     Console.Clear();
@@ -66,91 +67,9 @@ namespace math_opp
 
         // All methods under this line are only temporary
 
-        private void ohmLaw()
-        {
-
-            Console.WriteLine(@"
-
-            Ohms Law is used to calculate the Voltage, Current, Resistance, & Power within a circuit. It is usualy depicted like this.
-
-            Voltage = Current * Resistance (input => sv)
-
-            or
-
-            Power = Voltage * Current (input => sp)
-            ");
-
-            var oppChoice = "";
-
-            while (oppChoice == "")
-            {
-
-                Console.WriteLine("     Which opperation do you want to solve?");
-
-                var option = Console.ReadLine().ToLower();
 
 
-                if (option == "sv")
-                {
-                    voltSolve();
-                }
-                else if (option == "sp")
-                {
-                    Console.WriteLine("     You chose to solve for Power");
-                }
-                else
-                {
-                    Console.WriteLine("     Invalid User Imput");
-                }
 
-            }
-
-
-        }
-
-        private void voltSolve()
-        {
-            Console.WriteLine(@"
-
-            You chose to solve for Voltage
-
-            Voltage = Current * Resistance
-
-            For this application, you will be required to fill in values for Current (I) & Resistance (R)
-
-
-            ");
-
-            var valCurrNum = 0.0;
-
-            while (valCurrNum == 0.0)
-            {
-                Console.WriteLine("     Input value for I (Current)..");
-
-                var valCurrent = Console.ReadLine();
-
-                if (!double.TryParse(valCurrent, out valCurrNum))
-                {
-                    Console.WriteLine("Error! That isnt a Number");
-                }
-
-            }
-
-            // var valAnum = 0.0;
-
-            // while (valAnum == 0.0)
-            // {
-            //     Console.WriteLine("    Input value for A..");
-
-            //     var valA = Console.ReadLine();
-
-            //     if (!double.TryParse(valA, out valAnum))
-            //     {
-            //         Console.WriteLine("Error! That isnt a Number");
-            //     }
-            // }
-
-        }
 
 
 
--- app/OhmLaw.cs
+++ app/OhmLaw.cs
@@ -4,7 +4,120 @@ namespace math_opp
 {
     class OhmLaw
     {
+        private App app;
 
+        public OhmLaw(App _app)
+        {
+            this.app = _app;
+        }
+
+        public void ohmLaw()
+        {
+
+            Console.WriteLine(@"
+
+            Ohms Law is used to calculate the Voltage, Current, Resistance, & Power within a circuit. It is usualy depicted like this.
+
+            Voltage = Current * Resistance (input => sv)
+
+            or
+
+            Power = Voltage * Current (input => sp)
+
+            ");
+
+            var oppChoice = "";
+
+            while (oppChoice == "")
+            {
+
+                Console.WriteLine("     Which opperation do you want to solve?");
+
+                var option = Console.ReadLine().ToLower();
+
+
+                if (option == "sv")
+                {
+                    voltSolve();
+                    oppChoice = option;
+                }
+                else if (option == "sp")
+                {
+                    Console.WriteLine("     You chose to solve for Power");
+                    oppChoice = option;
+                }
+                else
+                {
+                    Console.WriteLine("     Invalid User Imput");
+                }
+
+            }
+
+
+        }
+
+        private void voltSolve()
+        {
+            Console.WriteLine(@"
+
+            You chose to solve for Voltage
+
+            Voltage = Current * Resistance
+
+            For this application, you will be required to fill in values for Current (I) & Resistance (R)
+
+            ");
+
+            var valCurrNum = 0.0;
+
+            while (valCurrNum == 0.0)
+            {
+                Console.WriteLine("     Input value for I (Current)..");
+
+                var valCurrent = Console.ReadLine();
+
+                if (!double.TryParse(valCurrent, out valCurrNum))
+                {
+                    Console.WriteLine("Error! That isnt a Number");
+                }
+
+            }
+
+            Console.WriteLine("     Your value for Current is " + valCurrNum);
+
+            var valResNum = 0.0;
+
+            while (valResNum == 0.0)
+            {
+                Console.WriteLine("     Input value for R (Resistance)..");
+
+                var valResist = Console.ReadLine();
+
+                if (!double.TryParse(valResist, out valResNum))
+                {
+                    Console.WriteLine("Error! That isnt a Number");
+                }
+
+            }
+
+            Console.WriteLine("     Your value for Resistance is " + valResNum);
+
+            var voltSum = ohmConvert(valCurrNum, valResNum);
+
+            Console.WriteLine("     Voltage is " + voltSum);
+
+            Console.WriteLine("     Continue? y/n");
+
+            var contChoice = Console.ReadLine();
+
+            app.continueChoice(contChoice);
+
+        }
+
+        private static double ohmConvert(double valI, double valR)
+        {
+            return valI * valR;
+        }
     }
 
 }
